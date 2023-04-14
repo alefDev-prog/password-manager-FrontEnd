ce0a540ce5888c4dca3f706711c2889916373434
alefCS50 <allan.efendic@icloud.com>
2023-04-14 17:44:23 +0200

    feat: contact with backend

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,6 @@
 'use client';
-import React from 'react';
+import React, { FormEvent } from 'react';
+import axios from 'axios';
 
 import { useState } from 'react';
 const Register = () => {
@@ -9,22 +10,38 @@ const Register = () => {
 
     function setUser(e: any): void {
         setUsername(e.target.value);
-        console.log(e.target.value);
-
     }
     function setPass(e: any):void {
-        setUsername(e.target.value);
+        setPass(e.target.value);
     }
+
+    async function sumbitFunc(e:any) {
+        e.preventDefault();
+
+        const resp = await fetch('http://localhost:5000/register',{
+            method:"POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+                username, password
+            }),
+        });
+
+    };
 
 
     return (
         <main id="register-main">
             <h1>Register</h1>
-            <form action="" method='post'>
+            <form onSubmit={sumbitFunc}>
                 <input type="text" name="username" onChange={setUser}/>
                 <input type="password" name="password" placeholder="password" onChange={setPass}/>
-                <button type="submit" value="submit" />
+                <div><button type="submit">Submit</button></div>
+                
             </form>
+            <div><h1>hej</h1></div>
+            
         </main>
     )
 }
