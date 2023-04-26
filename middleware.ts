2c590af1a47c7d2c588247796c972d3928ce3070
alefCS50 <allan.efendic@icloud.com>
2023-04-26 17:51:21 +0200

    fix: cookies

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,13 @@ import cookie from 'cookie';
 
 export function middleware(request: any) {
     const cookies = cookie.parse(request.headers.cookie || ''); // parse cookies from request headers
+    console.log(cookies);
     
-    if(cookies != undefined && (request.url.includes('register') || request.url.includes('login'))) {
+    if(Object.keys(cookies).length !== 0 && (request.url.includes('register') || request.url.includes('login'))) {
         return NextResponse.redirect(`${process.env.FRONTEND_URL}`+'data');
     }
 
-    if(cookies == undefined && request.url.includes('data')) {
+    if(Object.keys(cookies).length === 0 && request.url.includes('data')) {
         return NextResponse.redirect(`${process.env.FRONTEND_URL}`+'login');
     }
     
