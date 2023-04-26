fa4a581adb7e5c6b8f4ba42c1710cdbb7d485291
alefCS50 <allan.efendic@icloud.com>
2023-04-26 17:30:19 +0200

    fix: cookie get

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,14 @@ import { NextResponse } from "next/server";
 
 
 export function middleware(request: any) {
-    const cookie = request.cookies.get(process.env.COOKIE_NAME);
+    const cookie = request.cookies.get(`${process.env.COOKIE_NAME}`);
     
     if(cookie != undefined && (request.url.includes('register') || request.url.includes('login'))) {
-        return NextResponse.redirect(process.env.FRONTEND_URL+'data');
+        return NextResponse.redirect(`${process.env.FRONTEND_URL}`+'data');
     }
 
     if(cookie == undefined && request.url.includes('data')) {
-        return NextResponse.redirect(process.env.FRONTEND_URL+'login');
+        return NextResponse.redirect(`${process.env.FRONTEND_URL}`+'login');
     }
     
     
