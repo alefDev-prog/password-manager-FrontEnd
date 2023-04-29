3e34a6724886591524b633507d6ec396864e70a7
alefCS50 <allan.efendic@icloud.com>
2023-04-29 14:55:45 +0200

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,20 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import cookie from 'cookie';
 
 
 export function middleware(request: any) {
+
     const cookies = cookie.parse(request.headers.cookie || ''); // parse cookies from request headers
     const loggedInCookie = cookies.logged_in;
+
+    const {origin} = request.nextUrl;
     
     if(loggedInCookie && (request.url.includes('register') || request.url.includes('login'))) {
-        return NextResponse.redirect(`${process.env.FRONTEND_URL}`+'data');
+        return NextResponse.rewrite(origin+'/data');
     }
 
     if(!loggedInCookie && request.url.includes('data')) {
-        return NextResponse.redirect(`${process.env.FRONTEND_URL}`+'login');
+        return NextResponse.rewrite(origin+'/login');
     }
     
     
