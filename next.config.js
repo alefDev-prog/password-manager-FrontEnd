2c521c3e471ff951af2a3e19f5f5578f32fa0eb9
alefCS50 <allan.efendic@icloud.com>
2023-04-30 19:15:09 +0200

    fix: next config

--- next.config.js
+++ next.config.js
@@ -8,51 +8,7 @@ const nextConfig = {
     BACKEND_URL: process.env.BACKEND_URL
   },
 
- 
 
-  async rewrites() {
-    return [
-      { 
-        source: "/api/login",
-        destination: `${process.env.BACKEND_URL}/login`
-      
-      },
-      {
-        source: "/api/data",
-        destination: `${process.env.BACKEND_URL}/data`
-      
-      },
-      {
-        source: "/api/add",
-        destination:`${process.env.BACKEND_URL}/add`
-      
-      },
-      {
-        source: "/api/logout",
-        destination: `${process.env.BACKEND_URL}/logout`
-
-
-      },
-      {
-        source: "/api/delete",
-        destination: `${process.env.BACKEND_URL}/delete`
-
-
-      },
-      {
-        source: "/api/getpass",
-        destination: `${process.env.BACKEND_URL}/getpass`
-        
-        
-      },
-      {
-        source: "/api/register",
-        destination: `${process.env.BACKEND_URL}/register`
-        
-        
-      },
-    ];
-  }
 }
 
 module.exports = nextConfig
