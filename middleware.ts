d7ef8bcb81ae911ec0fca3b558540b77877e0f15
alefCS50 <allan.efendic@icloud.com>
2023-05-26 18:40:49 +0200

    fix: zooming on input, mobile

--- middleware.ts
+++ middleware.ts
@@ -6,14 +6,13 @@ export function middleware(request: any) {
     
     
     const loggedInCookie = request.cookies.get('logged_in')?.value;
-
     const {origin} = request.nextUrl;
     
     if(loggedInCookie && (request.url.includes('register') || request.url.includes('login'))) {
         return NextResponse.redirect(origin+'/data');
     }
 
-    if(loggedInCookie && request.url == origin) {
+    if(loggedInCookie && !request.url.includes('data')) {
         return NextResponse.redirect(origin+'/data');
     }
 
